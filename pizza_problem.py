8df2f483433b9195da359477e8049c6164d8db97
SURYA1804 <155455542+SURYA1804@users.noreply.github.com>
2024-04-23 11:55:34 +0530

    Add files via upload

---
+++ pizza_problem.py
@@ -0,0 +1,99 @@
+class cart:
+  def __init__(self,count,name,price):
+    self.count=count
+    self.name=name
+    self.price=price
+class discount:
+  def __init__(self,pizza,toppings,drink,discountrate):
+    self.pizza=pizza
+    self.toppings=toppings
+    self.drink=drink
+    self.discountrate=discountrate
+dis=discount('regular pizza','corn','pepsi',0.05)
+
+def discounts():
+  for i in range(0,len(c)):
+    if dis.pizza in c[i].name:
+      for i in range(0,len(c)):
+        if dis.toppings in c[i].name:
+          for i in range(0,len(c)):
+            if dis.drink in c[i].name:
+              return dis.discountrate
+    else:
+      return 0
+class pizza:
+    def __init__(self,name,price):
+        self.name=name
+        self.price=price
+c=[]
+p=[pizza("regular pizza",50),pizza("whole wheat pizza",75)]
+while True:
+  print("------------------Available pizza----------------")
+  for i in range(0,len(p)):
+    print(p[i].name,":",p[i].price)
+  print("Enter 'next' to show other option.")
+  pchoice=input("Enter the pizza:")
+  if pchoice == 'next':
+    break;
+  count=int(input("Enter the count:"))
+  for i in range(0,len(p)):
+    if pchoice==p[i].name:
+      c.append(cart(count,p[i].name,p[i].price))
+
+class toppings:
+    def __init__(self,name,price):
+        self.name=name
+        self.price=price
+
+t=[toppings("mozzarella cheese",30),toppings("cheddar",35),toppings("spanish",20),toppings("corn",15),toppings("mushroom",15)]
+while True:
+  print("--------------Available Toppings-----------------")
+  for i in range(0,len(t)):
+    print(t[i].name,":",t[i].price)
+  print("Enter next to show other option.")
+  tchoice=input("Enter the toppings:")
+  if tchoice=='next':
+    break;
+  count=int(input("Enter the count:"))
+  for i in range(0,len(t)):
+    if tchoice==t[i].name:
+      c.append(cart(count,t[i].name,t[i].price))
+
+
+class drink:
+    def __init__(self,name,price):
+        self.name=name
+        self.price=price
+class calculation:
+        def calculation(self):
+                print("**************BILL***************")
+                print("You have selected:")
+                total=0
+                for i in range(0,len(c)):
+                  print("Item = ",c[i].name,"|| Price =",c[i].price,"|| Count = ",c[i].count,"|| Total = ",c[i].count*c[i].price)
+                  total=total+(c[i].count*c[i].price)
+                print("The final amount to pay:",total-(discounts()*total))
+                print("************+++++++*************")
+
+def isDrinkOptionSelected(selectedDrinkOption):
+    return selectedDrinkOption == 1
+drinkoption=int(input("If you need drink press 1  (OR)\nIf you dont need press 2: "))
+if(isDrinkOptionSelected(drinkoption)):
+    d=[drink("pepsi",15),drink("7-up",20),drink("coke",25)]
+    while True:
+      print("----------------Available Drinks----------------")
+      for i in range(0,len(d)):
+        print(d[i].name,":",d[i].price)
+      print("Enter next to show other option.")
+      dchoice=input("Enter the drink:")
+      if dchoice=='next':
+        break;
+      count=int(input("Enter the count:"))
+      for i in range(0,len(d)):
+        if dchoice==d[i].name:
+          c.append(cart(count,d[i].name,d[i].price))
+
+cal=calculation()
+cal.calculation()
+
+
